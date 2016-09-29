9424668bc6f77a5295da44b750fd35fe753b7a56
Bryan Huss <binaryburnout@rachelle-husss-macbook-pro.local>
2016-09-29 07:16:29 -0400

    Added Void to functions

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -4,9 +4,7 @@
     Bryan Huss 09/26/2016
  
 - You have volunteered to be the Coordinator for your town’s youth soccer league. As part of your job you need to divide the 18 children who have signed up for the league into three even teams - Dragons, Sharks and Raptors. In years’ past, the teams have been unevenly matched, so this year you are doing your best to fix that. For each child, you will have the following information: Name, height (in inches), whether or not they have played soccer before and their guardians’ names.
- 
 - The project has three major parts. For each part, choose ONLY from the tools we have covered in the courses so far:
- 
  1. Native types and collections to store data (Dictionaries, Arrays, Ints, Strings, etc...)
  2. Accessing, appending and counting Arrays
  3. Accessing Dictionaries and Arrays
@@ -14,21 +12,13 @@
  5. Use of comparison operators
  6. String Interpolation
  7. Use of comments
- 
 - Please don’t employ more advanced tools we haven’t covered yet, even if they are right for the job. (Yes, that means no classes, structs, tuples or filters on Project 1.) However, if you identify a place where a more advanced tool is appropriate, please mention that in a code comment.
- 
 - Part 1: We have provided information for the 18 players in the Player Info spreadsheet. Please choose an appropriate data type to store the information for each player. In addition, create an empty collection variable to hold all the players’ data. Once you have decided on what tools to use, manually enter the player data so it can be used in Part 2.
- 
 - Part 2: Create logic that can iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
- 
 - Also, if you would like to attain an “exceeds expectations” rating for this project, add logic to ensure that each teams’ average height is within 1.5 inches of the others.
- 
 - Part 3: Create logic that iterates through all three teams of players and generates a personalized letter to the guardians, letting them know which team their child has been placed on and when they should attend their first team team practice. As long as you provide the necessary information (player name, team name, guardians’ names, practice date/time), feel free to have fun with the content of the letter. The team practice dates/times are as follows: Dragons - March 17, 1pm, Sharks - March 17, 3pm, Raptors - March 18, 1pm
- 
 - When your complete code is run in a playground the letters should be visible in the right hand pane. If the code is run within an actual Xcode Project, the letters should be visible in the console.
- 
 - As always, meaningful and concise code comments are expected. Your code should be written and refined in an Xcode playground or Xcode project, but be sure to upload it to GitHub, as per the instructions provided in the Soccer Coordinator Video Instruction, linked below.
- 
 - Good luck and Happy Coding!
 */
 
@@ -75,7 +65,7 @@ let numberOfTeams: Int = 3
 /* Distributes players in passed dictionary evenly between the teams with given arguments:
  -players: Dictionary - Contains players to be distribued
  */
-func assignPlayers(players: [[String : String]]) {
+func assignPlayers(players: [[String : String]]) -> Void {
     for x in 0..<players.count {
         if (x % numberOfTeams == 0) {
             dragons.append(players[x])
@@ -94,7 +84,7 @@ func assignPlayers(players: [[String : String]]) {
  -teamName name: String - Name of team
  -firstPractice date: String - Date of first practice
  */
-func printLetters(for team: [[String : String]], teamName name: String, firstPractice date: String) {
+func printLetters(for team: [[String : String]], teamName name: String, firstPractice date: String) -> Void {
     print("LETTERS FOR TEAM: \(name)")
     for player in team {
         print("Dear \(player["guardians"]!). Your child, \(player["name"]!), has been placed on the \(name) team this year. The first will be held on \(date).")
